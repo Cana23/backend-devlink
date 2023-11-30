b0415d1582547589b3f09c1307f6bbfe195c31a2
Cana23 <ethanbarea@gmail.com>
2023-11-30 11:09:15 -0500

    a ver si funciona

--- server.js
+++ server.js
@@ -18,19 +18,19 @@ app.use('/uploads', express.static('uploads'));
 const port = 8082;
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  port: 3306,
-  password: "",
-  database: "devlink",
+    host: 'localhost',
+    user: 'root',
+    port: 3306,
+    password: '',
+    database: 'devlink',
 });
 
 connection.connect((err) => {
-  if (err) {
-    console.error("Error de conexión: " + err.stack);
-    return;
-  }
-  console.log("Conexión a la base de datos exitosa");
+    if (err) {
+        console.error('Error de conexión: ' + err.stack);
+        return;
+    }
+    console.log('Conexión a la base de datos exitosa');
 });
 
 const storage = multer.diskStorage({
@@ -154,14 +154,14 @@ app.get('/comentarios/:user', (req, res) => {
 app.get('/publicaciones', (req, res) => {
     const sql = 'SELECT publicaciones.*, Users.name AS usuario, Users.email AS correo FROM publicaciones INNER JOIN Users ON publicaciones.id_usuario = Users.id;';
 
-  connection.query(sql, (err, results) => {
-    if (err) {
-      console.error("Error fetching publicaciones:", err); // Add this line
-      res.status(500).send("Fallo al obtener publicaciones");
-    } else {
-      res.status(200).json(results);
-    }
-  });
+    connection.query(sql, (err, results) => {
+        if (err) {
+            console.error('Error fetching publicaciones:', err); // Add this line
+            res.status(500).send('Fallo al obtener publicaciones');
+        } else {
+            res.status(200).json(results);
+        }
+    });
 });
 
 app.post('/agregarPublicaciones', upload.single('img'), (req, res) => {
@@ -212,158 +212,67 @@ app.post('/agregarComentarios', (req, res) => {
 })
 
 // Ruta para dar like a una publicación
-app.post("/posts/:postId/like", (req, res) => {
-  const { postId, userId } = req.body;
+app.post('/posts/:postId/like', (req, res) => {
+    const { postId, userId } = req.body;
+    const sql = 'INSERT INTO me_gusta (id_publicacion, id_usuario, fecha_like) VALUES (?, ?, NOW())';
+    const values = [postId, userId];
 
-  if (!id_publicacion || !id_usuario) {
-    return res.status(400).send("Se requieren id_publicacion e id_usuario");
-  }
-
-  const sql =
-    "INSERT INTO me_gusta (id_publicacion, id_usuario, fecha_like) VALUES (?, ?, NOW())";
-  const values = [postId, userId];
-
-  connection.query(sql, values, (err, results) => {
-    if (err) {
-      res.status(500).send("Error al dar like a la publicación");
-    } else {
-      res.status(200).send("Like agregado a la publicación exitosamente");
-    }
-  });
+    connection.query(sql, values, (err, results) => {
+        if (err) {
+            res.status(500).send('Error al dar like a la publicación');
+        } else {
+            res.status(200).send('Like agregado a la publicación exitosamente');
+        }
+    });
 });
 
 // Ruta para quitar like a una publicación
-app.delete("/posts/:postId/unlike", (req, res) => {
-  const { postId, userId } = req.body;
-  const sql =
-    "DELETE FROM me_gusta WHERE id_publicacion = ? AND id_usuario = ?";
-  const values = [postId, userId];
+app.delete('/posts/:postId/unlike', (req, res) => {
+    const { postId, userId } = req.body;
+    const sql = 'DELETE FROM me_gusta WHERE id_publicacion = ? AND id_usuario = ?';
+    const values = [postId, userId];
 
-  connection.query(sql, values, (err, results) => {
-    if (err) {
-      res.status(500).send("Error al quitar like de la publicación");
-    } else {
-      res.status(200).send("Like eliminado de la publicación exitosamente");
-    }
-  });
+    connection.query(sql, values, (err, results) => {
+        if (err) {
+            res.status(500).send('Error al quitar like de la publicación');
+        } else {
+            res.status(200).send('Like eliminado de la publicación exitosamente');
+        }
+    });
 });
 
 // Ruta para dar like a un comentario
-app.post("/comentarios/:comentarioId/like", (req, res) => {
-  const { comentarioId, userId } = req.body;
-  const sql =
-    "INSERT INTO me_gusta (id_comentario, id_usuario, fecha_like) VALUES (?, ?, NOW())";
-  const values = [comentarioId, userId];
+app.post('/comentarios/:comentarioId/like', (req, res) => {
+    const { comentarioId, userId } = req.body;
+    const sql = 'INSERT INTO me_gusta (id_comentario, id_usuario, fecha_like) VALUES (?, ?, NOW())';
+    const values = [comentarioId, userId];
 
-  connection.query(sql, values, (err, results) => {
-    if (err) {
-      res.status(500).send("Error al dar like al comentario");
-    } else {
-      res.status(200).send("Like agregado al comentario exitosamente");
-    }
-  });
+    connection.query(sql, values, (err, results) => {
+        if (err) {
+            res.status(500).send('Error al dar like al comentario');
+        } else {
+            res.status(200).send('Like agregado al comentario exitosamente');
+        }
+    });
 });
 
 // Ruta para quitar like a un comentario
-app.delete("/comentarios/:comentarioId/unlike", (req, res) => {
-  const { comentarioId, userId } = req.body;
-  const sql = "DELETE FROM me_gusta WHERE id_comentario = ? AND id_usuario = ?";
-  const values = [comentarioId, userId];
+app.delete('/comentarios/:comentarioId/unlike', (req, res) => {
+    const { comentarioId, userId } = req.body;
+    const sql = 'DELETE FROM me_gusta WHERE id_comentario = ? AND id_usuario = ?';
+    const values = [comentarioId, userId];
 
-  connection.query(sql, values, (err, results) => {
-    if (err) {
-      res.status(500).send("Error al quitar like del comentario");
-    } else {
-      res.status(200).send("Like eliminado del comentario exitosamente");
-    }
-  });
+    connection.query(sql, values, (err, results) => {
+        if (err) {
+            res.status(500).send('Error al quitar like del comentario');
+        } else {
+            res.status(200).send('Like eliminado del comentario exitosamente');
+        }
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Servidor disponible en el puerto ${port}`);
-});
-
-
-// * Get User stuff (profile, comment, etc.)
-
-// Obtener información del perfil de un usuario
-app.get('/perfil/:userId', (req, res) => {
-  const { userId } = req.params;
-
-  try {
-    const sql = 'SELECT * FROM Users WHERE id = ?';
-    const values = [userId];
-
-    connection.query(sql, values, (err, results) => {
-      if (err) {
-        console.error('Error al obtener información del perfil:', err);
-        res.status(500).send('Error interno del servidor');
-      } else {
-        if (results.length > 0) {
-          const userProfile = results[0];
-          res.status(200).json(userProfile);
-        } else {
-          res.status(404).send('Usuario no encontrado');
-        }
-      }
-    });
-  } catch (error) {
-    console.error('Error general al procesar la solicitud:', error);
-    res.status(500).send('Error interno del servidor');
-  }
-});
-
-
-
-
-
-// Repositorios
-app.get('/github-repos/:username', async (req, res) => {
-  const username = req.params.username;
-
-  try {
-      const response = await axios.get(`https://api.github.com/users/${username}/repos`);
-      const repos = response.data;
-
-      res.status(200).json(repos);
-  } catch (error) {
-      console.error('Error al obtener repositorios de GitHub:', error.message);
-      res.status(500).send('Error al obtener repositorios de GitHub');
-  }
-});
-
-// Endpoint para obtener publicaciones de un usuario
-app.get('/publicaciones/:userId', (req, res) => {
-  const userId = req.params.userId;
-
-  const sql = 'SELECT * FROM publicaciones WHERE id_usuario = ?';
-  const values = [userId];
-
-  connection.query(sql, values, (err, results) => {
-      if (err) {
-          console.error('Error al obtener publicaciones del usuario:', err);
-          res.status(500).send('Error al obtener publicaciones del usuario');
-      } else {
-          res.status(200).json(results);
-      }
-  });
-});
-
-// Endpoint para obtener comentarios de un usuario
-app.get('/comentarios/:userId', (req, res) => {
-  const userId = req.params.userId;
-
-  const sql = 'SELECT * FROM comentarios WHERE id_usuario = ?';
-  const values = [userId];
-
-  connection.query(sql, values, (err, results) => {
-      if (err) {
-          console.error('Error al obtener comentarios del usuario:', err);
-          res.status(500).send('Error al obtener comentarios del usuario');
-      } else {
-          res.status(200).json(results);
-      }
-  });
+    console.log(`Servidor disponible en el puerto ${port}`);
 });
 
 
